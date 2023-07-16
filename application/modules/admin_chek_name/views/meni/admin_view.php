e406924512b073b2862795d96dd9899fcf3dd8bd
Kittichai Raksawong <kittichdev@gmail.com>
2023-07-16 19:33:59 +0700

    Check

--- application/modules/admin_chek_name/views/meni/admin_view.php
+++ application/modules/admin_chek_name/views/meni/admin_view.php
@@ -1,40 +1,7 @@
-<div class="page-breadcrumb">
-    <div class="row">
-        <div class="col-5 align-self-center">
-            <h4 class="page-title">import to excel</h4>
-        </div>
-        <div class="col-7 align-self-center">
-            <div class="d-flex align-items-center justify-content-end">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item">
-                            <a href="#">Home</a>
-                        </li>
-                        <li class="breadcrumb-item active" aria-current="page">import_excel</li>
-                    </ol>
-                </nav>
-            </div>
-        </div>
-    </div>
-</div>
-
 <div class="container-fluid">
     <div class=".card-group">
         <div class="row">
             <div class="col-12">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">import to excel</h4>
-                        <h6 class="card-subtitle">Browse your computer: (Max: 40MiB)</h6>
-                        <div class="controls">
-                            <input type="file" name="file" class="form-control" required="" aria-invalid="false">
-                            <div class="help-block"></div>
-                        </div>
-                        <div class="mt-2 d-flex justify-content-end">
-                            <button type="button" class="btn btn-success"><i class="fas fa-check"></i> Success</button>
-                        </div>
-                    </div>
-                </div>
                 <div class="card">
                     <div class="card-body">
                         <div class="m-t-40">
@@ -42,31 +9,31 @@
                                 <div class="mr-auto">
                                     <div class="form-group">
                                         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel1" style="display: none;" aria-hidden="true">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h4 class="modal-title" id="exampleModalLabel1">New message</h4>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <form>
-                                                    <div class="form-group">
-                                                        <label for="recipient-name" class="control-label">Recipient:</label>
-                                                        <input type="text" class="form-control" id="recipient-name1">
+                                            <div class="modal-dialog" role="document">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h4 class="modal-title" id="exampleModalLabel1">New message</h4>
+                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
                                                     </div>
-                                                    <div class="form-group">
-                                                        <label for="message-text" class="control-label">Message:</label>
-                                                        <textarea class="form-control" id="message-text1"></textarea>
+                                                    <div class="modal-body">
+                                                        <form>
+                                                            <div class="form-group">
+                                                                <label for="recipient-name" class="control-label">Recipient:</label>
+                                                                <input type="text" class="form-control" id="recipient-name1">
+                                                            </div>
+                                                            <div class="form-group">
+                                                                <label for="message-text" class="control-label">Message:</label>
+                                                                <textarea class="form-control" id="message-text1"></textarea>
+                                                            </div>
+                                                        </form>
                                                     </div>
-                                                </form>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                                <button type="button" class="btn btn-primary">Send message</button>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                        <button type="button" class="btn btn-primary">Send message</button>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </div>
                                     </div>
                                 </div>
                                 <div class="ml-auto">
@@ -129,14 +96,11 @@
                                 <thead>
                                     <tr>
                                         <th class="footable-sortable">เลขที่<span class="footable-sort-indicator"></span></th>
-                                        <th class="footable-sortable">ชื่อขสกุล<span class="footable-sort-indicator"></span></th>
-                                        <th class="footable-sortable">Email<span class="footable-sort-indicator"></span></th>
-                                        <th class="footable-sortable">เบอร์โทรศัพ<span class="footable-sort-indicator"></span></th>
-                                        <th class="footable-sortable">ชั้นเรียน<span class="footable-sort-indicator"></span></th>
-                                        <th class="footable-sortable">อายุ<span class="footable-sort-indicator"></span></th>
-                                        <th class="footable-sortable">ปีการศืกษา<span class="footable-sort-indicator"></span></th>
+                                        <th class="footable-sortable">ชื่อ-นามสกุล<span class="footable-sort-indicator"></span></th>
                                         <th class="footable-sortable">ห้อง<span class="footable-sort-indicator"></span></th>
-                                        <th class="footable-sortable">Act<span class="footable-sort-indicator"></span></th>
+                                        <th class="footable-sortable">มาเรียน<span class="footable-sort-indicator"></span></th>
+                                        <th class="footable-sortable">ขาดเรียน<span class="footable-sort-indicator"></span></th>
+                                        <th class="footable-sortable">เช็คการส่งงาน<span class="footable-sort-indicator"></span></th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -145,31 +109,37 @@
                                         <td>
                                             <a href="javascript:void(0)"> Genelia Deshmukh</a>
                                         </td>
-                                        <td>genelia@gmail.com</td>
-                                        <td>+123 456 789</td>
-                                        <td>ไม่ระบุ</td>
-                                        <td>23</td>
-                                        <td>ไม่ระบุ</td>
-                                        <td>ไม่ระบุ</td>
+                                        <td>2/3</td>
+                                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" style="width:60px"></td>
+                                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" style="width:60px"></td>
                                         <td>
-                                            <button type="button" class="btn btn-sm btn-icon btn-pure btn-outline delete-row-btn"><i class="ti-pencil text-warning" aria-hidden="true"></i></button>
-                                            <button type="button" class="btn btn-sm btn-icon btn-pure btn-outline delete-row-btn" data-toggle="tooltip" data-original-title="Delete"><i class="ti-close text-danger" aria-hidden="true"></i></button>
+                                            <div class="dropdown">
+                                                <button class="btn btn-success dropdown-toggle" type="button" data-toggle="dropdown">บันทึกการส่งงาน
+                                                    <span class="caret"></span></button>
+                                                <ul class="dropdown-menu">
+                                                    <li><a href="#">ผ่าน</a></li>
+                                                    <li><a href="#">ไม่ผ่าน</a></li>
+                                                </ul>
+                                            </div>
                                         </td>
                                     </tr>
                                     <tr class="footable-odd" style="">
-                                        <td><span class="footable-toggle"></span>2</td>
+                                        <td><span class="footable-toggle"></span>1</td>
                                         <td>
-                                            <a href="javascript:void(0)"> Arijit Singh</a>
+                                            <a href="javascript:void(0)"> Genelia Deshmukh</a>
                                         </td>
-                                        <td>arijit@gmail.com</td>
-                                        <td>+234 456 789</td>
-                                        <td>ไม่ระบุ</td>
-                                        <td>23</td>
-                                        <td>ไม่ระบุ</td>
-                                        <td>ไม่ระบุ</td>
+                                        <td>2/2</td>
+                                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" style="width:60px"></td>
+                                        <td><input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" style="width:60px"></td>
                                         <td>
-                                        <button type="button" class="btn btn-sm btn-icon btn-pure btn-outline delete-row-btn"><i class="ti-pencil text-warning" aria-hidden="true"></i></button>
-                                            <button type="button" class="btn btn-sm btn-icon btn-pure btn-outline delete-row-btn" data-toggle="tooltip" data-original-title="Delete"><i class="ti-close text-danger" aria-hidden="true"></i></button>
+                                            <div class="dropdown">
+                                                <button class="btn btn-success dropdown-toggle" type="button" data-toggle="dropdown">บันทึกการส่งงาน
+                                                    <span class="caret"></span></button>
+                                                <ul class="dropdown-menu">
+                                                    <li><a href="#">ผ่าน</a></li>
+                                                    <li><a href="#">ไม่ผ่าน</a></li>
+                                                </ul>
+                                            </div>
                                         </td>
                                     </tr>
                                 </tbody>
